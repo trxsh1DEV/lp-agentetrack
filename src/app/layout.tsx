72d49ec6bd7c20a87dc0b41b57f746f420b19d05
trxsh1DEV <yagosousa630@gmail.com>
2025-02-21 11:24:07 -0300

    feat: Improvements google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
-/* eslint-disable @next/next/next-script-for-ga */
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Script from "next/script"; // Importação necessária para scripts no Next.js
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import { Toaster } from "@/components/ui/toaster";
-import { GoogleTagManager } from "@next/third-parties/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -24,20 +23,46 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <script
-          async
+        {/* Google Tag Manager - HEAD */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-53QPWRBG');
+          `}
+        </Script>
+
+        {/* Google Analytics - GTag */}
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-16871047013"
-        ></script>
-        <script>
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
+            function gtag(){window.dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-16871047013');
           `}
-        </script>
+        </Script>
       </head>
       <body className={`${poppins.style} antialiased bg-slate-900`}>
+        {/* Google Tag Manager - NOSCRIPT (no <body>) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-53QPWRBG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <main>
           <Toaster />
           <Navbar />
